d535101321404eeb4b38041af1c3ec7774768a82
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-23 15:59:26 -0500

    comments model seed and routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 
-// const userRoutes = require('./user-routes.js');
-// const postRoutes = require('./post-routes');
+const userRoutes = require("./user-routes.js");
+const postRoutes = require("./post-routes");
 const commentRoutes = require("./comment-routes");
 const eventRoutes = require("./event-routes");
-// router.use('/users', userRoutes);
-// router.use('/posts', postRoutes);
-//router.use("/comments", commentRoutes);
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
 router.use("/events", eventRoutes);
 
 module.exports = router;
