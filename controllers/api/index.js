f756f74421fd2bbd06052cad68eea5bf37f27377
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-23 13:36:15 -0500

    Event Model , seeds , Event routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // const userRoutes = require('./user-routes.js');
 // const postRoutes = require('./post-routes');
-// const commentRoutes = require('./comment-routes');
-
+const commentRoutes = require("./comment-routes");
+const eventRoutes = require("./event-routes");
 // router.use('/users', userRoutes);
 // router.use('/posts', postRoutes);
-// router.use('/comments', commentRoutes);
+//router.use("/comments", commentRoutes);
+router.use("/events", eventRoutes);
 
 module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+drop database if exists myneighbours_db;
+
+create database myneighbours_db;
+
+-- mysql -h 127.0.0.1 -P 3310 -u root -p
