fc6452a32904cdc02a67c871ad70c4d4e4c61829
unknown <ghaliarose89@gmail.com>
2021-11-24 13:37:46 -0600

    LikedPost Model created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const postRoutes = require('./post-routes');
 const commentRoutes = require("./comment-routes");
 const eventRoutes = require("./event-routes");
 const neighbourhoodRoutes = require("./neighbourhood-routes");
-
+//const likedPostRouters = require ('./')
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
 router.use("/comments", commentRoutes);
