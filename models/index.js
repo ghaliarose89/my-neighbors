dca17bb02a44d4ec4b186f52e9b6f0b951312729
unknown <ghaliarose89@gmail.com>
2021-11-24 19:16:41 -0600

    post and user association

--- models/index.js
+++ models/index.js
@@ -5,4 +5,20 @@ const User = require("./User");
 const Neighbourhood = require("./Neighbourhood");
 const LikedPosts = require ('./LikedPosts');
 
+//assotiations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
+
+
+
+
+
+
+
 module.exports = { Comment, Event, Post, User, Neighbourhood , LikedPosts };
