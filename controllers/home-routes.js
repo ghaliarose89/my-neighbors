278f161f5ab9b180507c3739d47c67bddfaf4727
unknown <ghaliarose89@gmail.com>
2021-11-27 09:44:36 -0600

    post render

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,11 +2,51 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { User, Event } = require("../models");
 
-router.get("/", (req, res) => {
-	res.render("homepage");
-});
+// router.get("/", (req, res) => {
+// 	res.render("homepage");
+// });
 router.get("/signup", (req, res) => {
 	res.render("signup");
 });
+//getting all posts if the user loged in
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_details',
+        'title',
+        'created_at',
+        //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+    //   include: [
+    //     {
+    //       model: Comment,
+    //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    //       include: {
+    //         model: User,
+    //         attributes: ['username']
+    //       }
+    //     },
+    //     {
+    //       model: User,
+    //       attributes: ['username']
+    //     }
+    //   ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+  
+        res.render('homepage', {
+          posts,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
 
 module.exports = router;
