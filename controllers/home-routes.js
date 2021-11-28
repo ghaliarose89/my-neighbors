5e3351ee96373c42d24a449c434c8e7b095185e3
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-28 01:51:34 -0500

    page corrections

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
 const Neighborhood = require("../models/Neighborhood");
-const {Post,User,Comment,Event } = require('../models');
-
+const { Post, User, Comment, Event } = require("../models");
 
 // router.get("/", (req, res) => {
 // 	res.render("homepage");
@@ -26,44 +25,49 @@ router.get("/signup", (req, res) => {
 		});
 });
 //getting all posts if the user loged in
-router.get('/', (req, res) => {
-    console.log('======================');
-    Post.findAll({
-      attributes: [
-        'id',
-        'post_details',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id)'), 'comment_count']
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', ],
-          include: {
-            model: User,
-            attributes: ['first_name']
-          }
-        },
-        {
-          model: User,
-          attributes: ['first_name']
-        }
-      ]
-    })
-      .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        console.log(posts)
-        res.render('homepage', {
-          posts,
-          loggedIn: req.session.loggedIn
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-  
+router.get("/", (req, res) => {
+	console.log("======================");
+	Post.findAll({
+		attributes: [
+			"id",
+			"post_details",
+			"title",
+			"created_at",
+			[
+				sequelize.literal(
+					"(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id)"
+				),
+				"comment_count",
+			],
+		],
+		include: [
+			{
+				model: Comment,
+				attributes: ["id", "comment_text", "post_id", "user_id"],
+				include: {
+					model: User,
+					attributes: ["first_name"],
+				},
+			},
+			{
+				model: User,
+				attributes: ["first_name"],
+			},
+		],
+	})
+		.then((dbPostData) => {
+			const posts = dbPostData.map((post) => post.get({ plain: true }));
+			console.log(posts);
+			res.render("homepage", {
+				posts,
+				loggedIn: req.session.loggedIn,
+				user_first_name: req.session.first_name,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
 
 module.exports = router;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,21 +1,41 @@
-const loginBtn = document.querySelector('#login');
-async function loginForm (event){
-    event.preventDefault();
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
+const loginBtn = document.querySelector("#login");
+async function loginForm(event) {
+	event.preventDefault();
+	const email = document.querySelector("#email").value.trim();
+	const password = document.querySelector("#password").value.trim();
 
-    if(email&& password) {
-    const response = await fetch('/api/users/login',{
-        method:'post',
-        body:JSON.stringify({
-            email, password
-        }),
-        headers: {'Content-Type': 'application/json' }
-    });
-    if (response.ok){
-        document.location.replace('/');
-    } else alert(response.status);
-    }
-};
+	if (email && password) {
+		const response = await fetch("/api/users/login", {
+			method: "post",
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+			headers: { "Content-Type": "application/json" },
+		});
+		if (response.ok) {
+			document.location.replace("/");
+		} else alert(response.status);
+	}
+}
 
-loginBtn.addEventListener('click',loginForm);
+async function logout() {
+	const response = await fetch("/api/users/logout", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+	if (response.ok) {
+		console.log("Logged out");
+		document.location.replace("/");
+	} else {
+		alert(response.statusText);
+	}
+}
+if (loginBtn) {
+	loginBtn.addEventListener("click", loginForm);
+}
+if (document.getElementById("logout")) {
+	document.getElementById("logout").addEventListener("click", logout);
+}
