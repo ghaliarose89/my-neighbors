7815a1796d8cd44021e100b719f46ea2b0acecfa
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-29 01:10:22 -0500

    event cards

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -58,6 +58,7 @@ if (document.getElementById("logout")) {
 	// 	.addEventListener("click", accessProfile);
 }
 window.onload = function () {
+	//GET NEIGHBORHOODS
 	let neighborhoods = [];
 	loadNeighborhoodsData()
 		.then((data) => {
@@ -73,8 +74,58 @@ window.onload = function () {
 				document.getElementById("welcome_msg_holder").innerHTML =
 					"Welcome To " + neighborhood.neighborhood_name;
 			}
-			console.log(neighborhoods);
+
 			return data;
 		})
 		.then((err) => {});
+
+	//GET EVENTS
+	let events = [];
+	loadEventsData()
+		.then((data) => {
+			events = data;
+			console.log(events);
+			buildEventCards(events);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
+
+// <div class="col mb-5 h-100">
+// 	<div class="feature bg-primary bg-gradient text-white rounded-3 mb-3">
+// 		<i class="bi bi-calendar3"></i>
+// 	</div>
+// 	<h2 class="h5"></h2>
+/* <p class="mb-0">
+	Paragraph of text beneath the heading to explain the heading. Here is just a
+	bit more text.
+</p>; */
+// 	<p class="mb-0"></p>
+// </div>;
+function buildEventCards(events) {
+	//events[0];
+	for (let i = 0; i < events.length; i++) {
+		const evt = events[i];
+
+		let div1 = $("<div>").addClass("col mb-5 h-100");
+		let div2 = $("<div>").addClass(
+			"feature bg-primary bg-gradient text-white rounded-3 mb-3"
+		);
+		div2.html('<i class="bi bi-calendar3"></i>');
+		console.log(evt.event_title);
+		let h2 = $("<h2>").addClass("h5");
+		h2.html(evt.event_title);
+		let p2 = $("<p>").addClass("mb-0");
+		let details = evt.event_details;
+		details += "<br> <span class='fw-bold'>Start Date: " + evt.event_start_date;
+		details +=
+			"</span><br> <span class='fw-bold'> End Date: " + evt.event_end_date;
+		details += "</span";
+		p2.html(details);
+
+		div1.append(div2, h2, p2);
+		console.log(div1);
+		$("#eventCardContainer").append(div1);
+	}
+}
